cbadb496189428d0ea2cd361e0f9eab411e76717
dwjrgal <dwjrgl651@gmail.com>
2024-07-23 13:40:31 +0800

    about

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,7 +1,24 @@
 const About = () => {
   return (
     <>
-      <section></section>
+      <section>
+        <img src="" alt="" />
+        <div>
+          <h4>Curious about me? Here you have it:</h4>
+          <p>
+            I'm a designer turned full stack developer, passionate about
+            React.js and Node.js. I excel in blending technical and visual
+            aspects to craft exceptional digital products, prioritizing user
+            experience, precise design, and optimized code.
+          </p>
+          <p>
+            Since starting my web development journey in 2015, I've embraced
+            challenges and kept up with the latest tech trends. Now in my early
+            thirties, seven years in, I'm building cutting-edge web apps using
+            Next.js, TypeScript, Nestjs, Tailwindcss, Supabase, and more.
+          </p>
+        </div>
+      </section>
     </>
   );
 };
