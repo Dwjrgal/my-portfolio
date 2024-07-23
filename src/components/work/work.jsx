f74a4cd7a8e7b2a9e55faa799acc475b747ae20b
dwjrgal <dwjrgl651@gmail.com>
2024-07-23 22:22:15 +0800

    work section

--- src/components/work/work.jsx
+++ src/components/work/work.jsx
@@ -4,6 +4,17 @@ const Work = () =>{
         <section>
             <h4>Work</h4>
             <p>Some of the noteworthy projects I have built:</p>
+            <div>
+            <img src="" alt="" />
+            <div>
+                <h3>Ubcab</h3>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda sit cum tempora quas quod, velit minima veniam ipsum facere totam impedit voluptate consequatur asperiores quia saepe illo inventore, expedita qui!</p>
+                <div className="tags">
+                    
+                </div>
+
+            </div>
+            </div>
         </section>
         </>
     )
