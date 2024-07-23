91ca6443a9f6536e8ebf1061abc097a9a9126cd6
dwjrgal <dwjrgl651@gmail.com>
2024-07-23 22:03:48 +0800

    experience and work

---
+++ src/components/work/work.jsx
@@ -0,0 +1,10 @@
+const Work = () =>{
+    return(
+        <>
+        <section>
+            <h4>Work</h4>
+            <p>Some of the noteworthy projects I have built:</p>
+        </section>
+        </>
+    )
+}
