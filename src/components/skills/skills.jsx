c15dc0b33409e15918be28dcebebdc5389cda410
dwjrgal <dwjrgl651@gmail.com>
2024-07-24 12:56:37 +0800

    skills logos

--- src/components/skills/skills.jsx
+++ src/components/skills/skills.jsx
@@ -5,7 +5,20 @@ const Skills = () => {
         <p className=" bg-gray-300 w-24 rounded text-center">Skills</p>
         <h5>The skills, tools and technologies I am really good at</h5>
         <div>
-          <img src="./" alt="" />
+          <img className="h-10" src="./images/icon-javscript.svg" alt="" />
+          <img className="h-10" src="./images/icon-typescript.svg" alt="" />
+          <img className="h-10" src="./images/icon-react.svg" alt="" />
+          <img className="h-10" src="./images/icon-nextjs.svg" alt="" />
+          <img className="h-10" src="./images/Tech.png" alt="" />
+          <img className="h-10" src="./images/Vector.png" alt="" />
+          <img className="h-10" src="./images/Vercel.svg" alt="" />
+          <img className="h-10" src="./images/next.svg" alt="" />
+          <img className="h-10" src="./images/postgresql.svg" alt="" />
+          <img className="h-10" src="./images/icon-mongodb.svg" alt="" />
+          <img className="h-10" src="./images/Figma.png" alt="" />
+          <img className="h-10" src="./images/Cypress.svg" alt="" />
+          <img className="h-10" src="./images/Storybook.png" alt="" />
+          <img className="h-10" src="./images/git.png" alt="" />
         </div>
       </section>
     </>
