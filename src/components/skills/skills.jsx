e31ff456d30dd8e8d61bde48c0cc18b491b568a3
dwjrgal <dwjrgl651@gmail.com>
2024-07-24 12:32:29 +0800

    skills logos

--- src/components/skills/skills.jsx
+++ src/components/skills/skills.jsx
@@ -4,7 +4,9 @@ const Skills = () => {
       <section className="flex flex-col gap-8 items-center h-[560px] w-[1440px] my-28">
         <p className=" bg-gray-300 w-24 rounded text-center">Skills</p>
         <h5>The skills, tools and technologies I am really good at</h5>
-        <div></div>
+        <div>
+          <img src="./" alt="" />
+        </div>
       </section>
     </>
   );
