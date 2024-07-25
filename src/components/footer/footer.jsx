c0921eff469691acd33dc8f000ee22875e891720
dwjrgal <dwjrgl651@gmail.com>
2024-07-25 09:25:14 +0800

    footer section

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,13 +1,24 @@
-const Footer = () =>{
-    return(
-        <>
-        <section>
-            <h4>Get in touch</h4>
-            <p>What’s next? Feel free to reach out to me if you're looking for <br /> a developer, have a query, or simply want to connect.</p>
-            <textarea name="" id=""></textarea>
-        </section>
-        </>
-    )
-}
+const Footer = () => {
+  return (
+    <>
+      <section className="flex  flex-col items-center mt-40">
+        <h4 className=" bg-gray-300 w-24 h-[28px] rounded-lg text-center">
+          Get in touch
+        </h4>
+        <p>
+          What’s next? Feel free to reach out to me if you're looking for <br />{" "}
+          a developer, have a query, or simply want to connect.
+        </p>
+        <textare
+          className="bg-red-500"
+          name="gmail"
+          id="gmail"
+          rows="2"
+          cols="2"
+        ></textare>
+      </section>
+    </>
+  );
+};
 
 export default Footer;
--- src/components/work/work.jsx
+++ src/components/work/work.jsx
@@ -2,90 +2,160 @@ const Work = () => {
   return (
     <>
       <section className="flex flex-col gap-8 mt-8 items-center">
-        <h4  className=" bg-gray-300 w-24 rounded text-center">Work</h4>
+        <h4 className=" bg-gray-300 w-24 rounded text-center">Work</h4>
         <p>Some of the noteworthy projects I have built:</p>
         <div className="flex justify-between gap-20 items-center mx-60 mt-20">
-          <img className="w-[484px] h-[384px] rounded-lg" src="https://images.unsplash.com/photo-1721299417031-de890ff33b26?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+          <img
+            className="w-[484px] h-[384px] rounded-lg"
+            src="https://images.unsplash.com/photo-1721299417031-de890ff33b26?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt=""
+          />
           <div className="flex flex-col  justify-start gap-10 h-[384px]">
-          <h3 className="text-md font-bold">UBcab</h3>
+            <h3 className="text-md font-bold">UBcab</h3>
             <p>
               Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-               <br />Assumenda sit cum tempora quas quod, velit minima veniam ipsum <br />
+              <br />
+              Assumenda sit cum tempora quas quod, velit minima veniam ipsum{" "}
+              <br />
               facere totam impedit voluptate consequatur asperiores quia saepe
               illo inventore, expedita qui!
             </p>
             <div className="tags flex  flex-wrap gap-5 w-[480px] h-[100px]">
-                
-                <p  className=" bg-gray-300 w-24 h-[28px] rounded-lg text-center">React</p>
-                <p  className=" bg-gray-300 w-24 h-[28px] rounded-lg text-center">Next.js</p>
-                <p  className=" bg-gray-300 w-24 h-[28px] rounded-lg text-center">Typescript</p>
-                <p  className=" bg-gray-300 w-24  h-[28px] rounded-lg text-center">Nest.js</p>
-                <p  className=" bg-gray-300 w-24 h-[28px] rounded-lg text-center">PostreSQl</p>
-                <p  className=" bg-gray-300 w-24 h-[28px] rounded-lg text-center">TailwindCSS</p>
-                <p  className=" bg-gray-300 w-24 h-[28px] rounded-lg text-center">Figma</p>
-                <p  className=" bg-gray-300 w-24 h-[28px] rounded-lg text-center">Cypress</p>
-                <p  className=" bg-gray-300 w-24  h-[28px] rounded-lg text-center">Storybook</p>
-                <p  className=" bg-gray-300 w-24 h-[28px] rounded-lg text-center">Git</p>
+              <p className=" bg-gray-300 w-24 h-[28px] rounded-lg text-center">
+                React
+              </p>
+              <p className=" bg-gray-300 w-24 h-[28px] rounded-lg text-center">
+                Next.js
+              </p>
+              <p className=" bg-gray-300 w-24 h-[28px] rounded-lg text-center">
+                Typescript
+              </p>
+              <p className=" bg-gray-300 w-24  h-[28px] rounded-lg text-center">
+                Nest.js
+              </p>
+              <p className=" bg-gray-300 w-24 h-[28px] rounded-lg text-center">
+                PostreSQl
+              </p>
+              <p className=" bg-gray-300 w-24 h-[28px] rounded-lg text-center">
+                TailwindCSS
+              </p>
+              <p className=" bg-gray-300 w-24 h-[28px] rounded-lg text-center">
+                Figma
+              </p>
+              <p className=" bg-gray-300 w-24 h-[28px] rounded-lg text-center">
+                Cypress
+              </p>
+              <p className=" bg-gray-300 w-24  h-[28px] rounded-lg text-center">
+                Storybook
+              </p>
+              <p className=" bg-gray-300 w-24 h-[28px] rounded-lg text-center">
+                Git
+              </p>
             </div>
           </div>
         </div>
-
 
         <div className="flex justify-between gap-20 items-center mx-60 mt-20">
           <div className="flex flex-col  justify-start gap-10 h-[384px]">
-          <h3 className="text-md font-bold">UBcab</h3>
+            <h3 className="text-md font-bold">UBcab</h3>
             <p>
               Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-               <br />Assumenda sit cum tempora quas quod, velit minima veniam ipsum <br />
+              <br />
+              Assumenda sit cum tempora quas quod, velit minima veniam ipsum{" "}
+              <br />
               facere totam impedit voluptate consequatur asperiores quia saepe
               illo inventore, expedita qui!
             </p>
             <div className="tags flex  flex-wrap gap-5 w-[480px] h-[100px]">
-                
-                <p  className=" bg-gray-300 w-24 h-[28px] rounded-lg text-center">React</p>
-                <p  className=" bg-gray-300 w-24 h-[28px] rounded-lg text-center">Next.js</p>
-                <p  className=" bg-gray-300 w-24 h-[28px] rounded-lg text-center">Typescript</p>
-                <p  className=" bg-gray-300 w-24  h-[28px] rounded-lg text-center">Nest.js</p>
-                <p  className=" bg-gray-300 w-24 h-[28px] rounded-lg text-center">PostreSQl</p>
-                <p  className=" bg-gray-300 w-24 h-[28px] rounded-lg text-center">TailwindCSS</p>
-                <p  className=" bg-gray-300 w-24 h-[28px] rounded-lg text-center">Figma</p>
-                <p  className=" bg-gray-300 w-24 h-[28px] rounded-lg text-center">Cypress</p>
-                <p  className=" bg-gray-300 w-24  h-[28px] rounded-lg text-center">Storybook</p>
-                <p  className=" bg-gray-300 w-24 h-[28px] rounded-lg text-center">Git</p>
+              <p className=" bg-gray-300 w-24 h-[28px] rounded-lg text-center">
+                React
+              </p>
+              <p className=" bg-gray-300 w-24 h-[28px] rounded-lg text-center">
+                Next.js
+              </p>
+              <p className=" bg-gray-300 w-24 h-[28px] rounded-lg text-center">
+                Typescript
+              </p>
+              <p className=" bg-gray-300 w-24  h-[28px] rounded-lg text-center">
+                Nest.js
+              </p>
+              <p className=" bg-gray-300 w-24 h-[28px] rounded-lg text-center">
+                PostreSQl
+              </p>
+              <p className=" bg-gray-300 w-24 h-[28px] rounded-lg text-center">
+                TailwindCSS
+              </p>
+              <p className=" bg-gray-300 w-24 h-[28px] rounded-lg text-center">
+                Figma
+              </p>
+              <p className=" bg-gray-300 w-24 h-[28px] rounded-lg text-center">
+                Cypress
+              </p>
+              <p className=" bg-gray-300 w-24  h-[28px] rounded-lg text-center">
+                Storybook
+              </p>
+              <p className=" bg-gray-300 w-24 h-[28px] rounded-lg text-center">
+                Git
+              </p>
             </div>
           </div>
-          <img className="w-[484px] h-[384px] rounded-lg" src="https://images.unsplash.com/photo-1721299417031-de890ff33b26?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+          <img
+            className="w-[484px] h-[384px] rounded-lg"
+            src="https://images.unsplash.com/photo-1721299417031-de890ff33b26?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt=""
+          />
         </div>
-
 
         <div className="flex justify-between gap-20 items-center mx-60 mt-20">
-          <img className="w-[484px] h-[384px] rounded-lg" src="https://images.unsplash.com/photo-1721299417031-de890ff33b26?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+          <img
+            className="w-[484px] h-[384px] rounded-lg"
+            src="https://images.unsplash.com/photo-1721299417031-de890ff33b26?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt=""
+          />
           <div className="flex flex-col  justify-start gap-10 h-[384px]">
-          <h3 className="text-md font-bold">UBcab</h3>
+            <h3 className="text-md font-bold">UBcab</h3>
             <p>
               Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-               <br />Assumenda sit cum tempora quas quod, velit minima veniam ipsum <br />
+              <br />
+              Assumenda sit cum tempora quas quod, velit minima veniam ipsum{" "}
+              <br />
               facere totam impedit voluptate consequatur asperiores quia saepe
               illo inventore, expedita qui!
             </p>
             <div className="tags flex  flex-wrap gap-5 w-[480px] h-[100px]">
-                
-                <p  className=" bg-gray-300 w-24 h-[28px] rounded-lg text-center">React</p>
-                <p  className=" bg-gray-300 w-24 h-[28px] rounded-lg text-center">Next.js</p>
-                <p  className=" bg-gray-300 w-24 h-[28px] rounded-lg text-center">Typescript</p>
-                <p  className=" bg-gray-300 w-24  h-[28px] rounded-lg text-center">Nest.js</p>
-                <p  className=" bg-gray-300 w-24 h-[28px] rounded-lg text-center">PostreSQl</p>
-                <p  className=" bg-gray-300 w-24 h-[28px] rounded-lg text-center">TailwindCSS</p>
-                <p  className=" bg-gray-300 w-24 h-[28px] rounded-lg text-center">Figma</p>
-                <p  className=" bg-gray-300 w-24 h-[28px] rounded-lg text-center">Cypress</p>
-                <p  className=" bg-gray-300 w-24  h-[28px] rounded-lg text-center">Storybook</p>
-                <p  className=" bg-gray-300 w-24 h-[28px] rounded-lg text-center">Git</p>
+              <p className=" bg-gray-300 w-24 h-[28px] rounded-lg text-center">
+                React
+              </p>
+              <p className=" bg-gray-300 w-24 h-[28px] rounded-lg text-center">
+                Next.js
+              </p>
+              <p className=" bg-gray-300 w-24 h-[28px] rounded-lg text-center">
+                Typescript
+              </p>
+              <p className=" bg-gray-300 w-24  h-[28px] rounded-lg text-center">
+                Nest.js
+              </p>
+              <p className=" bg-gray-300 w-24 h-[28px] rounded-lg text-center">
+                PostreSQl
+              </p>
+              <p className=" bg-gray-300 w-24 h-[28px] rounded-lg text-center">
+                TailwindCSS
+              </p>
+              <p className=" bg-gray-300 w-24 h-[28px] rounded-lg text-center">
+                Figma
+              </p>
+              <p className=" bg-gray-300 w-24 h-[28px] rounded-lg text-center">
+                Cypress
+              </p>
+              <p className=" bg-gray-300 w-24  h-[28px] rounded-lg text-center">
+                Storybook
+              </p>
+              <p className=" bg-gray-300 w-24 h-[28px] rounded-lg text-center">
+                Git
+              </p>
             </div>
           </div>
         </div>
-       
-       
-
       </section>
     </>
   );
